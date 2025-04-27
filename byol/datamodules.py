92660618b09fdfa8b56f0528242e4e78db3e1243
Alex <alexandralwalls@gmail.com>
2025-04-27 14:07:20 +0100

    Fix plotting

--- byol/datamodules.py
+++ byol/datamodules.py
@@ -340,16 +340,6 @@ class FineTuning_DataModule(pl.LightningDataModule):
             pin_memory=self.pin_memory,
         )
         return loader
-    
-    def rgz_dataloader(self):
-        loader = DataLoader(
-            self.data["rgz"],
-            batch_size=self.batch_size,
-            num_workers=self.num_workers,
-            prefetch_factor=self.prefetch_factor,
-            pin_memory=self.pin_memory,
-        )
-        return loader
 
 
 class RGZ_DataModule_Finetune(FineTuning_DataModule):
@@ -469,14 +459,6 @@ class RGZ_DataModule_Finetune(FineTuning_DataModule):
                 train=False,
                 transform=self.test_transform,
             ).with_annotator_labels(self.label_dist, self.RA_dec)
-            self.data["rgz"] = RGZ108k(
-                "/share/nas2_3/awalls/fr-mccp/_data/rgz",
-                train=True,
-                transform=self.train_transform,
-                remove_duplicates=True,
-                cut_threshold=25,
-                mb_cut=True,
-            )
 
         self.data["test"] = OrderedDict(
             {
--- byol/post_evaluation.py
+++ byol/post_evaluation.py
@@ -197,8 +197,6 @@ def load_dataloader(stage, label_dist=None, RA_dec=None):
         dataloader = datamodule.calibration_dataloader()
     elif stage == "test_conf":
         dataloader = datamodule.test_conf_dataloader()
-    elif stage == "rgz":
-        dataloader = datamodule.rgz_dataloader()
     else:
         raise ValueError("Unsupported dataloader stage.")
     return dataloader
