2288c949a3fbcd9a1db91a18d29c1aed298a8d66
Alex <alexandralwalls@gmail.com>
2025-04-27 15:31:50 +0100

    Fix plotting

--- byol/post_evaluation.py
+++ byol/post_evaluation.py
@@ -747,7 +747,7 @@ def run_post_evaluation(run_id):
                               download=False,
                               aug_type="torchvision"
                               ).with_annotator_labels(label_dist, RA_dec)
-    alphas = [0.05, 0.04, 0.03, 0.02, 0.1]
+    alphas = [0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
     for alpha in alphas:
         calibration_set = create_calibration_set(model, mb_calibration, 1, label_dist, RA_dec)
         threshold = calculate_threshold(calibration_set, alpha)
