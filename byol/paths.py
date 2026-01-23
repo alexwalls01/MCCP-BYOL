9e504116c366601cb4238ba38e4841385663a6c2
Alex <alexandralwalls@gmail.com>
2026-01-23 16:07:29 +0000

    Change location of data directory and add data directory to .gitignore.

--- byol/paths.py
+++ byol/paths.py
@@ -12,7 +12,7 @@ class Path_Handler:
             "project", Path(__file__).resolve().parent.parent
         )  # i.e. this repo
 
-        path_dict["data"] = kwargs.get("data", path_dict["root"] / "_data")
+        path_dict["data"] = kwargs.get("data", path_dict["project"] / "_data")
 
         path_dict["files"] = kwargs.get("files", path_dict["project"] / "files")
         path_dict["main"] = kwargs.get("main", path_dict["project"] / "byol")
