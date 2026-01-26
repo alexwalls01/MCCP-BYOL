94fbab5ab8474eb2a73c4ddb51e64576f5aabe79
Alex <alexandralwalls@gmail.com>
2026-01-26 12:46:41 +0000

    Pass logits to F.cross_entropy instead of softmax.

--- byol/finetuning.py
+++ byol/finetuning.py
@@ -139,8 +139,7 @@ class FineTune(pl.LightningModule):
         # Load data and targets
         x, y, _ = batch
         logits = self.forward(x)
-        y_pred = logits.softmax(dim=-1)
-        loss = F.cross_entropy(y_pred, y, label_smoothing=0.1 if self.n_layers else 0)
+        loss = F.cross_entropy(logits, y, label_smoothing=0.1 if self.n_layers else 0)
         self.log("finetuning/train_loss", loss, on_step=False, on_epoch=True)
         return loss
 
