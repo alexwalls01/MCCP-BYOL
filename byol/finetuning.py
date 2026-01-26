9f6a86255c140eb325bcbac7374597490f11147c
Alex <alexandralwalls@gmail.com>
2026-01-26 14:25:06 +0000

    Fix logging for dataset length.

--- byol/finetuning.py
+++ byol/finetuning.py
@@ -107,12 +107,12 @@ class FineTune(pl.LightningModule):
         # Log size of data-sets #
         train_len = len(self.trainer.datamodule.train_dataloader().dataset)
         val_len = len(self.trainer.datamodule.val_dataloader().dataset)
-        self.log("dataset/train_size", train_len)
-        self.log("dataset/val_size", val_len)
-
-        for name, loader in zip(self.trainer.datamodule.data["test"].keys(),
-                        self.trainer.datamodule.test_dataloader()):
-            self.log(f"dataset/test_size_{name}", len(loader.dataset))
+        self.logger.experiment.log({
+            "dataset/train_size": train_len,
+            "dataset/val_size": val_len,
+            "dataset/test_size_confident": len(self.trainer.datamodule.test_dataloader()[0].dataset),
+            "dataset/test_size_uncertain": len(self.trainer.datamodule.test_dataloader()[1].dataset),
+        })
 
         self.train_acc = tm.Accuracy(
             task="multiclass", average="micro", threshold=0, num_classes=self.n_classes
