ed25c134d39aaacb14de27c1f74804a116984a17
Alex <alexandralwalls@gmail.com>
2026-01-28 13:22:05 +0000

    Remove label smoothing from loss calculation for hard labels.

--- byol/finetuning.py
+++ byol/finetuning.py
@@ -137,7 +137,7 @@ class FineTune(pl.LightningModule):
             loss = -(y * log_p).sum(dim=1).mean()
         else:
             # Hard labels
-            loss = F.cross_entropy(logits, y, label_smoothing=0.1 if self.n_layers else 0)
+            loss = F.cross_entropy(logits, y)
         self.log("finetuning/train_loss", loss, on_step=False, on_epoch=True)
         return loss
 
